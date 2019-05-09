9d8cea8ef800b647e269030420163fa0c2ed3a7b
rutgersmit <rutger.smit@gmail.com>
2019-05-09 12:53:36 +0200

    Delete sethostname.sh

--- sethostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mac=`cat /sys/class/net/eth0/address`
-mac1="${mac:$i:5}"
-hostname="raspitemp-${mac1//:}"
-
-raspi-config nonint do_hostname "$hostname"
-hostname -b "$hostname"
-systemctl restart avahi-daemon
