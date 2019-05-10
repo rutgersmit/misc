c0ae3d78de4e77b6f681e3f5cd3a918f456a2bea
Rutger <rutger.smit@gmail.com>
2019-05-10 23:51:08 +0200

    better hostname solution

--- pi/sethostname.sh
+++ pi/sethostname.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#mac=`cat /sys/class/net/eth0/address`
+#mac1="${mac:$i:5}"
 
-mac=`cat /sys/class/net/eth0/address`
-mac1="${mac:$i:5}"
-hostname="raspitemp-${mac1//:}"
+# lookup the location name based on mac address
+l=$(cat locations.txt | grep `cat /sys/class/net/eth0/address` | awk '{print $2}')
+hostname="rpi-t-${l}"
 
 raspi-config nonint do_hostname "$hostname"
 hostname -b "$hostname"
