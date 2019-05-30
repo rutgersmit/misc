31ba373f6a460bf26a10a2d042e22e9d9726c59d
rutgersmit <rutger.smit@gmail.com>
2019-05-30 23:03:36 +0200

    Update firstboot.sh
    Added python-setuptools

--- pi/firstboot.sh
+++ pi/firstboot.sh
@@ -1,5 +1,5 @@
 apt update
-apt install python-dev git -y
+apt install python-dev python-setuptools git -y
 git clone https://github.com/adafruit/Adafruit_Python_DHT
 cd Adafruit_Python_DHT
 python setup.py install
