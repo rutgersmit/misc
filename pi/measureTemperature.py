2815c2d10643ce7ac4ba305d8c780fea2ed0cb2a
rutgersmit <rutger.smit@gmail.com>
2019-06-04 08:48:12 +0200

    Update measureTemperature.py
    error handling

--- pi/measureTemperature.py
+++ pi/measureTemperature.py
@@ -3,8 +3,14 @@ import sys
 from ds18b20 import DS18B20
 
 def gettemperature():
-    sensor = DS18B20()
-    t = sensor.tempC(0)
-    if  t > 100: # mis read
+    try:
+        sensor = DS18B20()
+
         t = sensor.tempC(0)
-    return t
+        if  t > 100: # mis read
+            t = sensor.tempC(0)
+
+        return t
+
+    except:
+        return 666
