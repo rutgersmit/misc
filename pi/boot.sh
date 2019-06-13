30fceae793479321910f384f69fa233c7c1cd856
rutgersmit <rutger.smit@gmail.com>
2019-06-13 23:32:46 +0200

    Update boot.sh

--- pi/boot.sh
+++ pi/boot.sh
@@ -2,5 +2,5 @@
 files=( "ds18b20.py" "config.py" "measure.py" "measureHumidity.py" "measureTemperatureSW.py" "measureTemperatureI2C.py")
 for i in "${files[@]}"
 do
-	wget -q https://raw.githubusercontent.com/rutgersmit/misc/master/pi/$i /home/pi/$i
+	wget -q https://raw.githubusercontent.com/rutgersmit/misc/master/pi/$i -o /home/pi/$i
 done
