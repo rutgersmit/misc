90307ae564f319b6d79d0509d0a3c188e7810d9d
Rutger <rutger@denieuwezaak.nl>
2019-06-13 23:19:01 +0200

    Update boot.sh
    improved startup script

--- pi/boot.sh
+++ pi/boot.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-echo "Hi!"
+#!/bin/bash
+files=( "ds18b20.py" "config.py" "measure.py" "measureHumidity.py" "measureTemperatureSW.py" "measureTemperatureI2C.py")
+for i in "${files[@]}"
+do
+	wget -q https://raw.githubusercontent.com/rutgersmit/misc/master/pi/$i /home/pi/$i
+done
