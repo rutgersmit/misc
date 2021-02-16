75f7cef7b89f47745abe3d052c4d8d358bfe4329
Rutger Smit <rutger.smit@gmail.com>
2021-02-16 23:56:54 +0100

    Wallpaper changes

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 URL="https://www.bing.com"$(curl -s 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=de-DE' | jq -r '.images[0].url')
 #echo $URL
-wget -q  -O /tmp/wallpaper.jpg $URL
-gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.jpg
+wget -O $HOME/Pictures/wallpaper.jpg $URL
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/wallpaper.jpg
 
