b1bb12170c537a264c0abdfada82b773af640187
Rutger Smit <rutger.smit@gmail.com>
2021-08-25 15:10:03 +0200

    Update wallpaper.sh
    Added revert option and removed locale (images are almost always the same)

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
 # en-US, ja-JP, en-AU, en-UK, de-DE, en-NZ, en-CA
-echo 'Which locale? Choose us, jp, au, de, nz or ca or wait 10 sec'
-read -t 10 l
-case $l in 
-    us)
-    l='en-US'
-    ;;
-    jp)
-    l='ja-JP'
-    ;;
-    au)
-    l='en-AU'
-    ;;
-    de)
-    l='de-DE'
-    ;;
-    nz)
-    l='en-NZ'
-    ;;
-    ca)
-    l='en-CA'
-    ;;
-    *)
-    l='de-DE'
-    ;;
-esac
+# echo 'Which locale? Choose us, jp, au, de, nz or ca or wait 10 sec'
+# read -t 3 l
+# case $l in 
+#     us)
+#     l='en-US'
+#     ;;
+#     jp)
+#     l='ja-JP'
+#     ;;
+#     au)
+#     l='en-AU'
+#     ;;
+#     de)
+#     l='de-DE'
+#     ;;
+#     nz)
+#     l='en-NZ'
+#     ;;
+#     ca)
+#     l='en-CA'
+#     ;;
+#     *)
+#     l='de-DE'
+#     ;;
+# esac
+l='en-US'
+
 
 URL="https://www.bing.com"$(curl -s 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=$l' | jq -r '.images[0].url')
-wget -q -O $HOME/Pictures/wallpaper.jpg $URL
+wget -q -O $HOME/Pictures/wallpaper-new.jpg $URL
 
 if command -v convert &> /dev/null
 then
-    convert $HOME/Pictures/wallpaper.jpg -filter Gaussian -blur 0x3 $HOME/Pictures/wallpaper-blur.jpg
+    convert $HOME/Pictures/wallpaper-new.jpg -filter Gaussian -blur 0x3 $HOME/Pictures/wallpaper-blur.jpg
 fi
 
-gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/wallpaper.jpg
+wallpaper=$(gsettings get org.gnome.desktop.background picture-uri)
+gsettings set org.gnome.desktop.background picture-uri file://$HOME/Pictures/wallpaper-new.jpg
+
+echo -n "Nice wallpaper? (Y/n)? "
+read answer
+if [ "$answer" != "${answer#[Nn]}" ] ;then
+    echo "Ok, reverting to the previous"
+else
+    cp -r $HOME/Pictures/wallpaper-new.jpg $HOME/Pictures/wallpaper.jpg
+    wallpaper="file://$HOME/Pictures/wallpaper.jpg"
+fi
+
+rm $HOME/Pictures/wallpaper-new.jpg
+
+gsettings set org.gnome.desktop.background picture-uri $wallpaper
